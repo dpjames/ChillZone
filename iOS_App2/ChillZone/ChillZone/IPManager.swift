c95c4e7a9009d8231d06f4fbead0c0ba444a1fbb
David James <davidjames@davids-mac-mini.home>
2018-02-21 01:20:39 -0800

    app chagnes

---
+++ iOS_App2/ChillZone/ChillZone/IPManager.swift
@@ -0,0 +1,12 @@
+//
+//  IPManager.swift
+//  ChillZone
+//
+//  Created by David James on 2/21/18.
+//  Copyright © 2018 David James. All rights reserved.
+//
+
+import Foundation
+class IPManager{
+    static var IP : String = "http://192.168.1.21";
+}
